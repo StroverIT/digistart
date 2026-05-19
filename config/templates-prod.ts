d052b619c4d6793ca4b6a86b3fa4215651e74be3
StroverIT <xcreaw1@abv.bg>
2026-05-19 10:58:11 +0300

    Add Additional Clothing Templates to Production Configuration
    - Expanded the templatesProd configuration by adding new clothing templates (2 to 8) with corresponding preview paths and rewrite targets.
    - This update enhances the template offerings available for users, improving the overall functionality and user experience in the application.

--- config/templates-prod.ts
+++ config/templates-prod.ts
@@ -11,5 +11,33 @@ export const templatesProd: TemplatesConfig = {
       previewPath: "/preview/clothing/1",
       rewriteTarget: "https://template-1-steel.vercel.app",
     },
+    "clothing:2": {
+      previewPath: "/preview/clothing/2",
+      rewriteTarget: "https://template-2-steel.vercel.app",
+    },
+    "clothing:3": {
+      previewPath: "/preview/clothing/3",
+      rewriteTarget: "https://template-3-steel.vercel.app",
+    },
+    "clothing:4": {
+      previewPath: "/preview/clothing/4",
+      rewriteTarget: "https://template-4-steel-three.vercel.app",
+    },
+    "clothing:5": {
+      previewPath: "/preview/clothing/5",
+      rewriteTarget: "https://template-5-steel-cyan.vercel.app/",
+    },
+    "clothing:6": {
+      previewPath: "/preview/clothing/6",
+      rewriteTarget: "https://template-6-steel.vercel.app/",
+    },
+    "clothing:7": {
+      previewPath: "/preview/clothing/7",
+      rewriteTarget: "https://template-7-steel.vercel.app/",
+    },
+    "clothing:8": {
+      previewPath: "/preview/clothing/8",
+      rewriteTarget: "https://template-8-steel.vercel.app/",
+    },
   },
 };
